ccaa6011409dfe35dca3482bea0ce52116501433
byongshintv <rnaos0530@gmail.com>
2021-06-23 11:32:10 +0900

    add printonly option

--- src/index.js
+++ src/index.js
@@ -9,49 +9,73 @@ const { StringUtil } = require('./util')
 const filePath = {
     setting: path.resolve(__dirname, '../setting.json'),
     question: path.resolve(__dirname, '../question.md'),
-    case: path.resolve(__dirname, './testcase.json'),
+    case: path.resolve(__dirname, './temp/testcase.json'),
 }
 
 const setting = getLiveJSON(filePath.setting, {
     input: "main.cpp",
-    output: "main"
+    output: "main",
+    lang: "cpp"
 })
+
+
+async function compile(isClear){
+
+    if (isClear) Printer.compile.loading(setting.get("input"), isClear)
+    const compileResult = await execAsync(
+        setting.get("compilerPath"),
+        ['-o', setting.get("output"), setting.get("input")]
+    )
+
+    if (compileResult[0]) {
+        Printer.compile.complete(false)
+        return false;
+    }
+    if (isClear) {
+        Printer.clear()
+        Printer.problem.title()
+    }
+    Printer.compile.complete(true)
+    return true;
+}
+
+async function test(){
+    const additionalCase = setting.get("testcase")
+    let testCases = JSON.parse(fs.readFileSync(filePath.case, 'utf-8'));
+    testCases = testCases.map((v, i) => [...v, `test case ${i + 1}`])
+
+    testCases = [].concat(testCases, additionalCase.map((v, i) => [...v, `user case ${i + 1}`]))
+
+    for (let [testInput, testOutput, label] of testCases) {
+        let [result, time] = await execIO(testInput,`./${setting.get("output")}.exe`);
+        result = StringUtil.removeLastSpace(result);
+        testOutput = StringUtil.removeLastSpace(testOutput);
+
+        const isOK = result == testOutput
+        Printer.testcase.title(label, isOK, time)
+
+        if (!isOK)
+            Printer.testcase.expected(
+                StringUtil.escapeNewline(testOutput),
+                StringUtil.escapeNewline(result)
+            )
+    }
+}
+
+async function execAndPrint(){
+    let testCases = JSON.parse(fs.readFileSync(filePath.case, 'utf-8'));
+
+    let [result, time] = await execIO(testCases[0][0],`./${setting.get("output")}.exe`);
+    console.log(result);
+}
+
 
 async function main() {
     async function triggeredSource(isClear = true) {
-        const additionalCase = setting.get("testcase")
-
-        if (isClear) Printer.compile.loading(setting.get("input"), isClear)
-        const compileResult = await execAsync(
-            setting.get("compilerPath"),
-            ['-o', setting.get("output"), setting.get("input")]
-        )
-
-        if (compileResult[0]) return Printer.compile.complete(false)
-        if (isClear) {
-            Printer.clear()
-            Printer.problem.title()
-        }
-        Printer.compile.complete(true)
-
-        let testCases = JSON.parse(fs.readFileSync(filePath.case, 'utf-8'));
-        testCases = testCases.map((v, i) => [...v, `test case ${i + 1}`])
-
-        testCases = [].concat(testCases, additionalCase.map((v, i) => [...v, `user case ${i + 1}`]))
-        for (let [testInput, testOutput, label] of testCases) {
-            let [result, time] = await execIO(testInput,`./${setting.get("output")}.exe`);
-            result = StringUtil.removeLastSpace(result);
-            testOutput = StringUtil.removeLastSpace(testOutput);
-
-            const isOK = result == testOutput
-            Printer.testcase.title(label, isOK, time)
-
-            if (!isOK)
-                Printer.testcase.expected(
-                    StringUtil.escapeNewline(testOutput),
-                    StringUtil.escapeNewline(result)
-                )
-        }
+        const compileResult = await compile(true);
+        if(!compileResult) return
+        if(setting.get("printOnly")) return execAndPrint();
+        await test();
     }
 
     watch.file(setting.get("input"), () => triggeredSource())
