85a0c1c3bf0275f235a44f8f0ad90a9d8b9769cf
byongshintv <rnaos0530@gmail.com>
2022-03-07 11:44:12 +0900

    문제 못받아오는 이슈 해결

--- src/request.js
+++ src/request.js
@@ -16,7 +16,25 @@ Array.prototype.chunk = function(n=2) {
 
 async function getProblemData(no = 1010){
     const url = 'https://www.acmicpc.net/problem/' + no
-    const raw = await fetch(url).then(res => res.text())
+    const raw = await fetch(url,{
+        headers: {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
+            "Accept-Encoding": "gzip, deflate, br", 
+            "Accept-Language": "ko-KR,ko;q=0.9", 
+            "Connection": "keep-alive", 
+            "Host": "www.acmicpc.net", 
+            "sec-ch-ua": `" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"`, 
+            "sec-ch-ua-mobile": "?0", 
+            "sec-ch-ua-platform": "Windows", 
+            "Sec-Fetch-Dest": "document", 
+            "Sec-Fetch-Mode": "navigate", 
+            "Sec-Fetch-Site": "none", 
+            "Sec-Fetch-User": "?1", 
+            "Upgrade-Insecure-Requests": "1", 
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36", 
+        },
+        redirect: 'follow',
+    }).then(res => res.text())
     const $raw = $(raw)
     $raw.find("img").attr("src",(i,v) => v.match(/^\//)? "https://www.acmicpc.net" +v : v)
 
